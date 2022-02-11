017e1fb6b88613ec139362f197620ae2447aac19
A21 <a21@3ds.com>
2022-02-11 15:27:21 +0530

    first commit

---
+++ src/test/java/com/example/rest/webservices/restfulwebservices/RestfulwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
